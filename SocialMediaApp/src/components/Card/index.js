15bc08f633e17e41b59123ae8f6bb90c33d868e9
Batuhan Kacmaz <batuhan.kacmaz1105@gmail.com>
2022-05-16 01:46:15 +0300

    update navigation.navigate function

--- SocialMediaApp/src/components/Card/index.js
+++ SocialMediaApp/src/components/Card/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
 import styles from './Card.style';
+import {useFetchUser} from '../../hooks/fetchData';
+
 const NewsCard = ({cardData, navigation, navigatePage}) => {
   console.log('items', cardData);
+  const {userData, userError, userLoading} = useFetchUser(cardData.id);
+  console.log('userData', userData);
   function navigateToPage() {
-    navigation.navigate(navigatePage, cardData);
+    navigation.navigate(navigatePage, userData);
   }
 
   return (
